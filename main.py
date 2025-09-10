c461abda983aa905bb5db7a76da6ca7178c40a77
Sebastián Solorzano <sol21826@uvg.edu.gt>
2025-09-10 17:38:05 -0600

    feat: demo

---
+++ main.py
@@ -0,0 +1,7 @@
+from mathlib.mathlib import square, factorial, is_prime, gcd, lcm
+
+print("square(5) =", square(5))
+print("\nfactorial(2) =", factorial(0))
+print("\nis_prime(2) =", is_prime(2))
+print("\ngcd(54, 24) =", gcd(54, 24))
+print("\nlcm(4, 6) =", lcm(4, 6))
